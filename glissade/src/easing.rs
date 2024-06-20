3ff5f20ce293cb854270f73c1731908211812695
Andrei Monkin <monkin.andrey@gmail.com>
2024-06-20 23:26:37 +0300

    Add easing images to the documentation

--- glissade/src/easing.rs
+++ glissade/src/easing.rs
@@ -2,19 +2,52 @@
 /// See: [https://easings.net/](https://easings.net/) for more information.
 #[derive(Clone, Copy, Debug, PartialEq, Default)]
 pub enum Easing {
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMCIgd2lkdGg9IjEwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgZmlsbD0icmdiYSgwLCAwLCAwLCAwLjEyKSIvPgogIDxwb2x5Z29uIHBvaW50cz0iMCwgMTAwIDEwMCwgMCIgc3R5bGU9InN0cm9rZTogYmxhY2s7IHN0cm9rZS13aWR0aDogMTsgZmlsbDogbm9uZTsiIC8+Cjwvc3ZnPg=="/>
+    /// </div>
     Linear,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSw5OS43NSAxMCw5OSAxNSw5Ny43NSAyMCw5NiAyNSw5My43NSAzMCw5MSAzNSw4Ny43NSA0MCw4NCA0NSw3OS43NSA1MCw3NSA1NSw2OS43NSA2MCw2NCA2NSw1Ny43NSA3MCw1MSA3NSw0My43NSA4MCwzNiA4NSwyNy43NSA5MCwxOSA5NSw5Ljc1IDEwMCwwIiBzdHlsZT0ic3Ryb2tlOiBibGFjazsgc3Ryb2tlLXdpZHRoOiAxOyBmaWxsOiBub25lOyIgLz4KPC9zdmc+"/>
+    /// </div>
     QuadraticIn,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSw5MC4yNSAxMCw4MSAxNSw3Mi4yNSAyMCw2NCAyNSw1Ni4yNSAzMCw0OSAzNSw0Mi4yNSA0MCwzNiA0NSwzMC4yNSA1MCwyNSA1NSwyMC4yNSA2MCwxNiA2NSwxMi4yNSA3MCw5IDc1LDYuMjUgODAsNCA4NSwyLjI1IDkwLDEgOTUsMC4yNSAxMDAsMCIgc3R5bGU9InN0cm9rZTogYmxhY2s7IHN0cm9rZS13aWR0aDogMTsgZmlsbDogbm9uZTsiIC8+Cjwvc3ZnPg=="/>
+    /// </div>
     QuadraticOut,
-    QuadraticInOut,
-    CubicIn,
-    CubicOut,
-    CubicInOut,
-    QuarticIn,
-    QuarticOut,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSw5OS41IDEwLDk4IDE1LDk1LjUgMjAsOTIgMjUsODcuNSAzMCw4MiAzNSw3NS41IDQwLDY4IDQ1LDU5LjUgNTAsNTAgNTUsNDAuNSA2MCwzMiA2NSwyNC41IDcwLDE4IDc1LDEyLjUgODAsOCA4NSw0LjUgOTAsMiA5NSwwLjUgMTAwLDAiIHN0eWxlPSJzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDE7IGZpbGw6IG5vbmU7IiAvPgo8L3N2Zz4="/>
+    /// </div>
     #[default]
+    QuadraticInOut,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSwxMDAgMTAsOTkuODc1IDE1LDk5LjYyNSAyMCw5OS4yNSAyNSw5OC41IDMwLDk3LjI1IDM1LDk1Ljc1IDQwLDkzLjYyNSA0NSw5MC44NzUgNTAsODcuNSA1NSw4My4zNzUgNjAsNzguMzc1IDY1LDcyLjUgNzAsNjUuNzUgNzUsNTcuODc1IDgwLDQ4Ljc1IDg1LDM4LjYyNSA5MCwyNy4xMjUgOTUsMTQuMjUgMTAwLDAiIHN0eWxlPSJzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDE7IGZpbGw6IG5vbmU7IiAvPgo8L3N2Zz4="/>
+    /// </div>
+    CubicIn,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSw4NS43NSAxMCw3Mi44NzUgMTUsNjEuMzc1IDIwLDUxLjI1IDI1LDQyLjI1IDMwLDM0LjI1IDM1LDI3LjUgNDAsMjEuNjI1IDQ1LDE2LjYyNSA1MCwxMi41IDU1LDkuMTI1IDYwLDYuMzc1IDY1LDQuMjUgNzAsMi43NSA3NSwxLjYyNSA4MCwwLjc1IDg1LDAuMzc1IDkwLDAuMTI1IDk1LDAgMTAwLDAiIHN0eWxlPSJzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDE7IGZpbGw6IG5vbmU7IiAvPgo8L3N2Zz4="/>
+    /// </div>
+    CubicOut,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSwxMDAgMTAsOTkuNjI1IDE1LDk4LjYyNSAyMCw5Ni43NSAyNSw5My43NSAzMCw4OS4yNSAzNSw4Mi44NzUgNDAsNzQuMzc1IDQ1LDYzLjUgNTAsNTAgNTUsMzYuNSA2MCwyNS42MjUgNjUsMTcuMTI1IDcwLDEwLjc1IDc1LDYuMjUgODAsMy4yNSA4NSwxLjM3NSA5MCwwLjM3NSA5NSwwIDEwMCwwIiBzdHlsZT0ic3Ryb2tlOiBibGFjazsgc3Ryb2tlLXdpZHRoOiAxOyBmaWxsOiBub25lOyIgLz4KPC9zdmc+"/>
+    /// </div>
+    CubicInOut,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSwxMDAgMTAsMTAwIDE1LDEwMCAyMCw5OS44NzUgMjUsOTkuNjI1IDMwLDk5LjI1IDM1LDk4LjUgNDAsOTcuNSA0NSw5NS44NzUgNTAsOTMuNzUgNTUsOTAuODc1IDYwLDg3IDY1LDgyLjEyNSA3MCw3NiA3NSw2OC4zNzUgODAsNTkgODUsNDcuNzUgOTAsMzQuMzc1IDk1LDE4LjUgMTAwLDAiIHN0eWxlPSJzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDE7IGZpbGw6IG5vbmU7IiAvPgo8L3N2Zz4="/>
+    /// </div>
+    QuarticIn,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSw4MS41IDEwLDY1LjYyNSAxNSw1Mi4yNSAyMCw0MSAyNSwzMS42MjUgMzAsMjQgMzUsMTcuODc1IDQwLDEzIDQ1LDkuMTI1IDUwLDYuMjUgNTUsNC4xMjUgNjAsMi41IDY1LDEuNSA3MCwwLjc1IDc1LDAuMzc1IDgwLDAuMTI1IDg1LDAgOTAsMCA5NSwwIDEwMCwwIiBzdHlsZT0ic3Ryb2tlOiBibGFjazsgc3Ryb2tlLXdpZHRoOiAxOyBmaWxsOiBub25lOyIgLz4KPC9zdmc+"/>
+    /// </div>
+    QuarticOut,
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwxMDAgNSwxMDAgMTAsOTkuODc1IDE1LDk5LjYyNSAyMCw5OC43NSAyNSw5Ni44NzUgMzAsOTMuNSAzNSw4OCA0MCw3OS41IDQ1LDY3LjI1IDUwLDUwIDU1LDMyLjc1IDYwLDIwLjUgNjUsMTIgNzAsNi41IDc1LDMuMTI1IDgwLDEuMjUgODUsMC4zNzUgOTAsMC4xMjUgOTUsMCAxMDAsMCIgc3R5bGU9InN0cm9rZTogYmxhY2s7IHN0cm9rZS13aWR0aDogMTsgZmlsbDogbm9uZTsiIC8+Cjwvc3ZnPg=="/>
+    /// </div>
     QuarticInOut,
     Step(f32),
     Bezier(f32, f32, f32, f32),
+    /// <div>
+    ///     <img style="width: 102px; height: 102px;" src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMiIgd2lkdGg9IjEwMiIgdmlld0JveD0iLTEgLTEgMTAyIDEwMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB4PSItMSIgeT0iLTEiIHdpZHRoPSIxMDIiIGhlaWdodD0iMTAyIiBmaWxsPSJyZ2JhKDAsIDAsIDAsIDAuMTIpIi8+CiAgPHBvbHlsaW5lIHBvaW50cz0iMCwwIDEwMCwwIiBzdHlsZT0ic3Ryb2tlOiBibGFjazsgc3Ryb2tlLXdpZHRoOiAxOyBmaWxsOiBub25lOyIgLz4KPC9zdmc+"/>
+    /// </div>
     None,
 }
 
